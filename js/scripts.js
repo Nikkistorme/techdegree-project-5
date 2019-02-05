af38d083ba419c6915bc907ddd475bd2c8b1cbc1
Nicholas Bright <nikkistorme@gmail.com>
2019-02-05 10:14:30 -0600

    finished scripts for search bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 
 $(".search-bar").on("keyup", function() {
   const $input = $(".search-bar").val().toLowerCase();
-  const $photoSet = $("a[href*='photos']";
+  const $photoSet = $("a[href*='photos']");
 
   for(let i=0; i< $photoSet.length; i+=1) {
-    console.log($photoSet[i]);
+    const $descript = $($photoSet[i]).attr("data-title").toLowerCase();
+    if ($descript.indexOf($input) > -1) {
+      $photoSet[i].style.display = "block";
+    } else {
+      $photoSet[i].style.display = "none";
+    }
+    console.log($input);
   }
 });
